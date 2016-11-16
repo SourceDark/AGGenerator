13168f8f5b06942e569f368f62b060448cd6254a
胡天翔 <sourcedark@gmail.com>
2016-11-16 13:53:51 +0800

    finishi generation of image "nmap v1.0"

--- sensor/images/nmap/scan.cpp
+++ sensor/images/nmap/scan.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <cstring>
 using namespace std;
 
 /**
@@ -28,11 +29,12 @@ map<string, string> configs;
 void load_configs() {
 	ifstream conf; 
 	conf.open("config.ini");
-	char s[101];
 	int line_number = 0;
-	while (conf.getline(s, 100)) {
+	string str;
+	while (getline(conf, str)) {
+		while (str[str.length() - 1] == '\n' || str[str.length() - 1] == '\r' || str[str.length() - 1] == ' ') str = str.substr(0, str.length() - 1);
+		while (str[0] == '\n' || str[0] == '\r' || str[0] == ' ') str = str.substr(1, str.length() - 1);
 		line_number ++;
-		string str(s);
 		int i = str.find("=");
 		if (i == string::npos) {
 			logger << "Wrong config file at line " << line_number << ": " << str;
@@ -46,8 +48,7 @@ void load_configs() {
 }
 
 void load_scan_targets() {
-	ifstream stf; 
-	stf.open(configs[CONFIG_SCAN_CONFIGS_FILE].c_str());
+	ifstream stf(configs[CONFIG_SCAN_CONFIGS_FILE].c_str()); 
 	stf >> scan_targets;
 	stf >> scan_type;
 	stf.close();
@@ -57,11 +58,12 @@ void initial_program() {
 	logger.open("scan.log");
 	load_configs();
 	load_scan_targets();
+	cout << "Initial completed." << endl;
 }
 
 vector<string> analyze_light_probe_outputs() {
 	vector<string> ips;
-	ifstream nof("data/nmap.output");
+	ifstream nof("nmap.output");
 	string str, cip;
 	while (getline(nof, str)) {
 		if (str.find("Nmap scan report for ") != string::npos) {
@@ -80,7 +82,8 @@ vector<string> analyze_light_probe_outputs() {
 
 void main_work() {
 	if (scan_type == "light") {
-		string cmd = "nmap -v -sn " + scan_targets + " > data/nmap.output";
+		cout << "start light probing: " << scan_targets << endl;
+		string cmd = "nmap -v -sn " + scan_targets + " > nmap.output";
 		system(cmd.c_str());
 		vector<string> ips = analyze_light_probe_outputs();
 		ofstream srf(configs[CONFIG_SCAN_RESULTS_FILE].c_str());
