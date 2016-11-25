797a89a8281617c5b4f0fb83386af2f3da04f895
胡天翔 <sourcedark@gmail.com>
2016-11-25 17:15:09 +0800

    Now control can generate properiate input files for analysis algorithm

--- dockerfiles/mulval_analysis_control/control.php
+++ dockerfiles/mulval_analysis_control/control.php
@@ -1,5 +1,39 @@
 <?php
-$url = 'http://localhost:8000/api/algorithms/1/results';
-$html = file_get_contents($url);  
-echo $html;  
+/**
+ * Get configurations from file
+ */
+$input_file_content = file_get_contents("/data/input");
+$inputs = json_decode($input_file_content);
+$host_api = $inputs->host_api;
+$gen_alg_params = $inputs->input;
+
+/**
+ * Loading results of generation
+ */
+$gen_alg_result_content = file_get_contents("http://" . $host_api . "/api/algorithms/" . $gen_alg_params->algorithm_id . "/results/" . $gen_alg_params->result_id);
+$gen_alg_result = json_decode($gen_alg_result_content);
+$alg_inputs = json_decode($gen_alg_result->content);
+$nodes = $alg_inputs->nodes;
+$edges = $alg_inputs->edges;
+var_dump($nodes);
+var_dump($edges);
+
+/**
+ * Write input file for algorithm
+ */
+if (!is_dir('/data/algorithm')) {
+    mkdir('/data/algorithm');
+}
+$ouf = fopen('/data/algorithm/input', "w");
+fwrite($ouf, sizeof($nodes) . ' ' . sizeof($edges) . "\r\n");
+foreach ($nodes as $node) {
+    fwrite($ouf, $node->id . "\r\n");
+    fwrite($ouf, $node->info . "\r\n");
+    fwrite($ouf, $node->type . "\r\n");
+    fwrite($ouf, $node->initial . "\r\n");
+}
+foreach ($edges as $edge) {
+    fwrite($ouf, $edge->target . "\r\n");
+    fwrite($ouf, $edge->source . "\r\n");
+}
 ?>
--- dockerfiles/mulval_analysis_control/mulval_analysis.dockerfile
+++ dockerfiles/mulval_analysis_control/mulval_analysis.dockerfile
@@ -2,6 +2,7 @@ FROM php:7.0-cli
 
 # copy required files
 RUN mkdir /src
+WORKDIR /src
 RUN mkdir /data
 ADD control.php /src/control.php
 
