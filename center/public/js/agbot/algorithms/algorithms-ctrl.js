b852ce3258120a4dd9191ccd476171b7b76e11e8
胡天翔 <seeeeeeven7@gmail.com>
2016-12-19 15:34:26 +0800

    update index

--- center/public/js/agbot/algorithms/algorithms-ctrl.js
+++
@@ -1,6 +0,0 @@
-agbotApp.controller('algorithmsCtrl', function ($scope, $http) {
-    $scope.algorithms = {};
-    $http.get('/api/algorithms').success(function (response) {
-        $scope.algorithms = response;
-    });
-});
--- center/resources/views/agbot/algorithms/index.blade.php
+++ center/resources/views/agbot/algorithms/index.blade.php
@@ -1,12 +1,28 @@
-<div ng-controller="algorithmsCtrl">
-    <div class="container">
-        <h3>Attack Graph Generation Algorithms</h3>
-        <ul>
-            <li ng-repeat="algorithm in algorithms.generation"><a ui-sref="algorithms.algorithm({algorithm_id:algorithm.id})" >{{algorithm.name}}</a></li>
-        </ul>
-        <h3>Attack Graph Analysis Algorithms</h3>
-        <ul>
-            <li ng-repeat="algorithm in algorithms.analysis"><a ui-sref="algorithms.algorithm({algorithm_id:algorithm.id})" >{{algorithm.name}}</a></li>
-        </ul>
+<div class="container">
+    <p class="route-path"></p>
+    <div class="board">
+        <table class="table">
+            <thead>
+            <tr>
+                <td>#</td>
+                <td>Name</td>
+                <td>Image</td>
+                <td>Input Type</td>
+                <td>Output Type</td>
+                <td>Updated At</td>
+            </tr>
+            </thead>
+            <tbody>
+
+            <tr ng-repeat="algorithm in algorithms">
+                <td>{{algorithm.id}}</td>
+                <td><a ui-sref="algorithms.algorithm({algorithm_id:algorithm.id})">{{algorithm.name}}</a></td>
+                <td>{{algorithm.image}}</td>
+                <td>{{algorithm.inputType}}</td>
+                <td>{{algorithm.outputType}}</td>
+                <td ng-bind="algorithm.updatedTime | date:'yyyy-MM-dd HH:mm:ss'"></td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </div>
