27af92e663acc01d750cd641dce24533efe3c952
xr <xierui.serc@gmail.com>
2016-12-20 11:17:27 +0800

    save

--- center-backend/src/main/java/org/serc/algorithm/support/ResultTypeRepository.java
+++ center-backend/src/main/java/org/serc/algorithm/support/ResultTypeRepository.java
@@ -3,6 +3,6 @@ package org.serc.algorithm.support;
 import org.serc.algorithm.model.ResultType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResultTypeRepository extends JpaRepository<ResultType, Long> {
+public interface ResultTypeRepository extends JpaRepository<ResultType, String> {
 
 }
