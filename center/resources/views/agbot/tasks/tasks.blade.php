8ffc9d6daaef4249b1f24c3032ec94dc8016a9bf
Steven Hu <seeeeeeven7@gmail.com>
2017-01-17 16:43:54 +0800

    update task

--- center/resources/views/agbot/tasks/tasks.blade.php
+++ center/resources/views/agbot/tasks/tasks.blade.php
@@ -8,20 +8,24 @@
                     <td>#</td>
                     <td>Algorithm</td>
                     <td>Container</td>
+                    <td>Content Type</td>
                     <td>Status</td>
                     <td>Updated At</td>
+                    <td>Operation</td>
                 </tr>
                 </thead>
                 <tbody>
                 <tr ng-repeat="task in tasks.content">
                     <td><a ui-sref="task.info({taskId:task.id})">{{task.id}}</a></td>
                     <td><a ui-sref="algorithms.algorithm({algorithm_id:task.algorithm.id})">{{task.algorithm.name}}</a></td>
-                    <td class="code">{{task.containerId}}</td>
+                    <td class="code">{{task.containerId.substr(0, 10)}}</td>
+                    <td>{{task.inputType}} => {{task.outputType}}</td>
                     <td ng-if="task.status=='running'"><a class="running" ui-sref="task.info({taskId:task.id})">Running</a></td>
                     <td ng-if="task.status=='created'"><a class="created" ui-sref="task.info({taskId:task.id})">Created</a></td>
                     <td ng-if="task.status=='success'"><a class="success" ui-sref="task.info({taskId:task.id})">Success</a></td>
                     <td ng-if="task.status=='failure'"><a class="fail" ui-sref="task.info({taskId:task.id})">Failure</a></td>
                     <td ng-bind="task.updatedTime | date:'yyyy-MM-dd HH:mm:ss'"></td>
+                    <td><a ng-if="task.outputType=='attack_graph2.0' || task.outputType=='attack_graph'" href="#/algorithms/{{task.algorithm.id}}/attack_graph/{{task.id}}" target="_blank"><i class="fa fa-eye" aria-hidden="true"></i></a></td>
                 </tr>
                 </tbody>
             </table>
