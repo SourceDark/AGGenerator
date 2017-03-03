278738deabbcb3da0b24663a54edeb6d2d8c621d
Nettle <nettle256@gmail.com>
2017-03-03 17:30:05 +0800

    work save

--- blur-center/src/app/pages/asset/asset.module.js
+++ blur-center/src/app/pages/asset/asset.module.js
@@ -9,7 +9,25 @@
         'BlurAdmin.pages.asset.information'
     ])
         .config(routeConfig)
-        .controller('assetCtrl', function($scope,$state) {
+        .controller('assetCtrl', function($scope,$state,$http) {
+            $scope.networkId = 1;
+            $scope.apiUrl = 'http://162.105.30.200:9016';
+
+            $http
+                .get([$scope.apiUrl, 'server', $scope.networkId, 'hosts'].join('/'))
+                .then(function (result) {
+                    $scope.sensorNames = $.unique(result.data.map(function (host) {
+                        return host.sensorName;
+                    }));
+                    $scope.hosts = {};
+                    $scope.sensorNames.forEach(function (sensor) {
+                        $scope.hosts[ sensor ] = result.data.filter(function (host) {
+                            return host.sensorName == sensor;
+                        })
+                    });
+                }, function (result) {
+                    console.error('获取资产信息失败');
+                });
         });
 
     /** @ngInject */
--- blur-center/src/app/pages/asset/information/information.module.js
+++ blur-center/src/app/pages/asset/information/information.module.js
@@ -6,13 +6,24 @@
     'use strict';
 
     angular.module('BlurAdmin.pages.asset.information', [])
-        .config(routeConfig);
+        .config(routeConfig)
+        .controller('assetInformationCtrl', function ($scope, $stateParams, $http) {
+            console.log($stateParams);
+            $http
+                .get([$scope.apiUrl, 'server', $scope.networkId, 'sensors', $stateParams.sensorName, 'hosts', $stateParams.ip].join('/'))
+                .then(function (result) {
+                    console.log(result.data);
+                }, function (result) {
+                    console.error('获取资产信息失败');
+                });
+        });
 
     /** @ngInject */
     function routeConfig($stateProvider) {
         $stateProvider
             .state('asset.information', {
                 url: '/information',
+                params:{'sensorName':null,ip:null},
                 templateUrl: 'app/pages/asset/information/information.html',
                 title: '资产详情'
             });
