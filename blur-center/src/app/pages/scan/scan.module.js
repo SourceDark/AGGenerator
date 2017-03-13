3d802895b80093b298655644df00ceb5d5f52ed4
Nettle <nettle256@gmail.com>
2017-03-13 16:59:38 +0800

    scan

--- blur-center/src/app/pages/scan/scan.module.js
+++ blur-center/src/app/pages/scan/scan.module.js
@@ -6,31 +6,23 @@
     'use strict';
 
     angular.module('BlurAdmin.pages.scan', [
-        'BlurAdmin.pages.scan.new'
+        'BlurAdmin.pages.scan.new',
+        'BlurAdmin.pages.scan.list'
     ])
+        .run(['$rootScope', '$state', function($rootScope, $state) {
+            $rootScope.$on('$stateChangeStart', function(evt, to, params) {
+                if (to.redirectTo) {
+                    evt.preventDefault();
+                    $state.go(to.redirectTo, params, {location: 'replace'})
+                }
+            });
+        }])
         .config(routeConfig)
         .controller('scanCtrl', function($scope,$state,$http) {
             $scope.networkId = 1;
             $scope.apiUrl = 'http://162.105.30.200:9016';
 
-            $http
-                .get([$scope.apiUrl, 'server', $scope.networkId, 'tasks'].join('/'))
-                .then(function (result) {
-                    $scope.tasks = result.data;
-                }, function () {
-
-                });
-
-            $scope.showLong = function (start, end) {
-                if (!end) return ;
-                var t = (end - start) / 1000;
-                var ret = (t % 60) + 's';
-                if ((parseInt(t / 60) % 60) > 0)
-                    ret = (parseInt(t / 60) % 60) + 'm' + ret;
-                if (parseInt(t / 3600) > 0)
-                    ret = parseInt(t / 3600) + 'h' + ret;
-                return ret;
-            }
+            $state.go('scan.list');
         });
 
     /** @ngInject */
@@ -43,8 +35,9 @@
                 sidebarMeta: {
                     icon: 'ion-search',
                     order: 3
-                }
-            })
+                },
+                redirectTo: 'scan.list'
+            });
     }
 
 })();
