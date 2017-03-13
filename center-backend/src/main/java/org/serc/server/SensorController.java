bf0f7119f1d7867b7784fbd7913d55bf7d9c1dd7
xr <xierui.serc@gmail.com>
2017-03-13 16:08:35 +0800

    save

---
+++ center-backend/src/main/java/org/serc/server/SensorController.java
@@ -0,0 +1,50 @@
+package org.serc.server;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.serc.exception.ResourceNotFoundException;
+import org.serc.network.controller.dto.CveEntry;
+import org.serc.network.controller.dto.HostDto;
+import org.serc.network.controller.dto.SensorListDto;
+import org.serc.network.model.Host;
+import org.serc.network.model.HostVulnerability;
+import org.serc.network.model.Network;
+import org.serc.network.support.NetworkUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Lists;
+
+@RestController
+@RequestMapping("/server/{network}/sensors")
+public class SensorController {
+    
+    @GetMapping("")
+    public List<SensorListDto> getSensors(Network network) {
+        return network.getSensors().stream().map(SensorListDto::new).collect(Collectors.toList());
+    }
+    
+    @GetMapping("/{sensorName}/hosts/{ip:[0-9\\.]*}")
+    public HostDto host(Network network, @PathVariable String sensorName, @PathVariable String ip) {
+        Host host = network.getSensors().stream().filter(s -> s.getName().equals(sensorName)).findFirst()
+                .orElseThrow(ResourceNotFoundException::new)
+                .getHosts().stream().filter(h -> h.getIp().trim().equals(ip)).findFirst()
+                .orElseThrow(ResourceNotFoundException::new);
+        List<CveEntry> cves = Lists.newArrayList();
+        HostDto hostDto = new HostDto(host.getIp(), sensorName);
+        NetworkUtils.setCveEntries(host, NetworkUtils.getCves(host));
+        for(HostVulnerability hostVulnerability: host.getVulnerabilities()) {
+            for(CveEntry vulnerability: hostVulnerability.getCveList()) {
+                hostDto.setScore(hostDto.getScore() + vulnerability.getCvssScore());
+                hostDto.setVulnerabilityCount(hostDto.getVulnerabilityCount() + 1);
+                cves.add(vulnerability);
+            }
+        }
+        hostDto.setVulnerabilities(cves);
+        return hostDto;
+    }
+
+}
--- center-backend/src/main/java/org/serc/server/ServerController.java
+++ center-backend/src/main/java/org/serc/server/ServerController.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.serc.exception.ResourceNotFoundException;
 import org.serc.network.controller.dto.CveEntry;
-import org.serc.network.controller.dto.HostDto;
 import org.serc.network.controller.dto.HostListDto;
 import org.serc.network.controller.dto.NetworkListDto;
 import org.serc.network.model.Host;
@@ -19,7 +17,6 @@ import org.serc.network.support.NetworkScheduleTaskRepository;
 import org.serc.network.support.NetworkUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -83,23 +80,4 @@ public class ServerController {
         return hosts;
     }
     
-    @GetMapping("/sensors/{sensorName}/hosts/{ip:[0-9\\.]*}")
-    public HostDto host(Network network, @PathVariable String sensorName, @PathVariable String ip) {
-        Host host = network.getSensors().stream().filter(s -> s.getName().equals(sensorName)).findFirst()
-                .orElseThrow(ResourceNotFoundException::new)
-                .getHosts().stream().filter(h -> h.getIp().trim().equals(ip)).findFirst()
-                .orElseThrow(ResourceNotFoundException::new);
-        List<CveEntry> cves = Lists.newArrayList();
-        HostDto hostDto = new HostDto(host.getIp(), sensorName);
-        NetworkUtils.setCveEntries(host, NetworkUtils.getCves(host));
-        for(HostVulnerability hostVulnerability: host.getVulnerabilities()) {
-            for(CveEntry vulnerability: hostVulnerability.getCveList()) {
-                hostDto.setScore(hostDto.getScore() + vulnerability.getCvssScore());
-                hostDto.setVulnerabilityCount(hostDto.getVulnerabilityCount() + 1);
-                cves.add(vulnerability);
-            }
-        }
-        hostDto.setVulnerabilities(cves);
-        return hostDto;
-    }
 }
