d381f62de653c3fbaf95e3cc1cdbcd3916a10698
xr <xierui.serc@gmail.com>
2017-04-13 11:57:05 +0800

    save

--- center-backend/src/main/java/org/serc/Application.java
+++ center-backend/src/main/java/org/serc/Application.java
@@ -2,10 +2,12 @@ package org.serc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Configuration
 @EnableAsync
 @EnableScheduling
 public class Application {
